d498f3192b6d761786f54972ff0ebcde3ff3005b
RosenTsankov <tsankov.rosen@gmail.com>
2021-05-27 13:06:40 +0300

    formatting

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,26 @@ import React from "react";
 import DayList from "./DayList/DayList";
 
 /**
- * App Components
+ * App Component
  */
 const App = () => (
   <div className="App">
       <header>
-          <h1>This is One-Shot Weather App Exercise</h1>
+          <h1>Weather App Exercise</h1>
       </header>
       <section>
           <ul>
-              <li>Provide 5 day weather forecast by city name (you can hardcode it) </li>
+              <li>Provide 5 day weather forecast by city name.</li>
               <li>OpenWeatherMap API Documentation: <a rel="noreferrer" href="http://openweathermap.org/forecast5" target="_blank">http://openweathermap.org/forecast5</a> </li>
+              <li>API_KEY: 00f024c81fe188ad264aedc20c7ec218</li>
+              <li>WEATHER_API_URI: https://api.openweathermap.org/data/2.5/forecast</li>
               <li>
-                  API_KEY: 00f024c81fe188ad264aedc20c7ec218
-                  WEATHER_API_URI: https://api.openweathermap.org/data/2.5/forecast
-
-                  /**
-                  * getWeatherForecast
-                  *
-                  * Sample Call:
-                  * https://api.openweathermap.org/data/2.5/forecast?q=London&appid=00f024c81fe188ad264aedc20c7ec218&units=metric
-                  */
+                  Sample Call:
+                  <br />
+                  <a target="_blank" href="https://api.openweathermap.org/data/2.5/forecast?q=London&appid=00f024c81fe188ad264aedc20c7ec218&units=metric">
+                      https://api.openweathermap.org/data/2.5/forecast?q=London&appid=00f024c81fe188ad264aedc20c7ec218&units=metric
+                  </a>
               </li>
-              <li>API Key is provided in /api.js</li>
               <li>Use any supporting technologies, package management, build systems, component libraries that you are familiar with and feel are appropriate.</li>
               <li>You can use any 3-rd party library.</li>
           </ul>
